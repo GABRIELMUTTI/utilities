7d2da86bcd8024f26fcc8a6c80fa41df606a7245
GABRIELMUTTI <gabrielmuttiteixeira@gmail.com>
2019-04-07 11:31:33 -0300

    Changed static variables initialization to avoid multiple definition

--- include/utl/type/TypeInfo.hpp
+++ include/utl/type/TypeInfo.hpp
@@ -11,25 +11,42 @@ namespace utl
     class TypeInfo
     {
     private:
-	static uint numRegistered;
-
+	static inline uint& numRegistered()
+	{
+	    static uint numRegistered = 0;
+	    return numRegistered;
+	}
+	
 	template<typename T>
 	struct TypeId
 	{
-	    static const uint id;
-	    static uint numDerivedClasses;
+	    inline static const uint& id()
+	    {
+		static const uint id = registerType<T>();
+		return id;
+	    }
+
+	    inline static uint& numDerivedClasses()
+	    {
+		static uint numDerivedClasses = 0;
+		return numDerivedClasses;
+	    }
 	};
 
 	template<class TDerived, class TBase>
 	struct DerivedClassId
 	{
-	    static const uint id;
+	    inline static const uint& id()
+	    {
+		static const uint id = registerDerivedClass<TDerived, TBase>();
+		return id;
+	    }
 	};
 
 	template<typename T>
 	static uint registerType()
 	{
-	    return numRegistered++;
+	    return numRegistered()++;
 	}
 
 	template<class TDerived, class TBase>
@@ -42,37 +59,26 @@ namespace utl
 	template<typename T>
 	static uint getId()
 	{
-	    return TypeId<T>::id;
+	    return TypeId<T>::id();
 	}
 
 	template<typename T>
 	static uint getNumDerivedClasses()
 	{
-	    return TypeId<T>::numDerivedClasses;
+	    return TypeId<T>::numDerivedClasses();
 	}
 
 	template<class TDerived, class TBase>
 	static uint getDerivedClassId()
 	{
 	    static_assert(std::is_base_of<TBase, TDerived>::value, "TDerived does not inherit from TBase.");
-	    return DerivedClassId<TDerived, TBase>::id;
+	    return DerivedClassId<TDerived, TBase>::id();
 	}
 
 	template<typename T>
 	static Type getType()
 	{
-	    return Type(TypeId<T>::id, sizeof(T));
+	    return Type(TypeId<T>::id(), sizeof(T));
 	}
     };
-
-    uint TypeInfo::numRegistered = 0;
-
-    template<class T>
-    uint TypeInfo::TypeId<T>::numDerivedClasses = 0;
-    
-    template<typename T>
-    const uint TypeInfo::TypeId<T>::id = TypeInfo::registerType<T>();
-
-    template<class TDerived, class TBase>
-    const uint TypeInfo::DerivedClassId<TDerived, TBase>::id = TypeInfo::registerDerivedClass<TDerived, TBase>();
 }
